e0448371dad9434d4468383b5e4b2443e2c9e665
[tucubanit0] <[javiperez9801@gmail.com]>
2025-05-07 15:31:52 +0100

    start

---
+++ Modulo 1/06_05/prueba01.py
@@ -0,0 +1,8 @@
+a = 3
+b = 4
+suma = a + b
+
+print(suma)
+print(a - b)
+print(a * b)
+print(a / b)
---
+++ Modulo 1/07_05/actividad_evaluable.py
@@ -0,0 +1,33 @@
+# Crea una variable llamada nombre y asígnale tu nombre como un string. Luego, imprime la variable en la pantalla.
+"""nombre = 'Javier'
+print(nombre)"""
+
+#Crea dos variables llamadas a y b y asígnales los valores 3 y 4 respectivamente. 
+#Luego, crea una variable llamada c y asígnale el resultado de sumar a y b. Finalmente, imprime el valor de c en la pantalla.
+"""a = 3
+b = 4
+c = a + b
+print(c)"""
+
+# Crea una variable llamada pi y asígnale el valor 3.14. Luego, crea una variable llamada radio y asígnale el valor 5. 
+# Finalmente, crea una variable llamada área y asígnale el resultado de multiplicar pi por el cuadrado de radio. 
+# Imprime el valor de área en la pantalla.
+"""pi = 3.14
+radio = 5
+area = pi * (radio ** 2)
+print(area)"""
+
+# Crea una variable llamada x y asígnale el valor 10. Luego, crea una variable llamada y y asígnale el valor 2. 
+# Finalmente, crea una variable llamada z y asígnale el resultado de dividir x entre y. 
+# Imprime el valor de z en la pantalla.
+"""x = 10
+y = 2
+z = x / y
+print(z)"""
+
+# Crea una variable llamada temperatura_celsius y asígnale un valor numérico. 
+# Luego, crea una variable llamada temperatura_fahrenheit y asígnale el resultado de convertir la temperatura de Celsius a Fahrenheit utilizando la fórmula: (temperatura_celsius * 9/5) + 32. 
+# Finalmente, imprime el valor de temperatura_fahrenheit en la pantalla.
+temperatura_celsius = 30
+temperatura_fahrenheit = (temperatura_celsius * 9/5) + 32
+print(temperatura_fahrenheit)
